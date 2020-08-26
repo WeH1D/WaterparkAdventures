cc79f7201b79b84898528c470440f5b90f8a9288
WeH1D <haticv92@gmail.com>
2020-08-26 13:20:16 +0200

    screenshots added

---
+++ Assets/Scripts/cameraSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSwitch : MonoBehaviour
+{
+    public Camera cam1;
+    public Camera cam2;
+
+    public bool enable;
+
+    void Start()
+    {
+        enable = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            enable = !enable;
+            cam1.enabled = enable;
+            cam2.enabled = !enable;
+        }
+    }
+}
